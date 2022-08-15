3ff70fbe7cbfa7e00a45fa23a91970122a23ff68
Christophe HERVI <christophe.hervi@free.fr>
2022-08-15 03:03:24 +0200

    création de la classe évaluation

---
+++ Public/Modele/classe_Evaluation.php
@@ -0,0 +1,12 @@
+<?php
+class Evaluation { // un élève a été évalué sur l'activité X de l'évaluation Y à un moment t
+  private ID;
+  private nom;
+  private valeur;
+  
+  public function __construct(){}
+  public function __destruct(){}
+  // setter
+  // getter
+  // autre
+}
