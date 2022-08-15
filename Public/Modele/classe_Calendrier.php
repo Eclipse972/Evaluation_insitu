e346ee0522b3f20b8af19a6fe66c7d5bf8161a6d
Christophe HERVI <christophe.hervi@free.fr>
2022-08-15 03:09:41 +0200

    création de la classe calendrier

---
+++ Public/Modele/classe_Calendrier.php
@@ -0,0 +1,14 @@
+<?php
+class Calendrier { // un professeur fait une évaluation à un moment de la semaine
+  private ID;
+  private profID;
+  private debut; // timestamp?
+  private fin;
+  private evaluationID;
+  
+  public function __construct(){}
+  public function __destruct(){}
+  // setter
+  // getter
+  // autre
+}
