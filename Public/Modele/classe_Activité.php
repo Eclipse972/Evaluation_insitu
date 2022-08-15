ea53e9770e1d330c1810e5ca68c9a8aea9273421
Christophe HERVI <christophe.hervi@free.fr>
2022-08-15 03:42:13 +0200

    ajout d'une description pour l'activité

--- Public/Modele/classe_Activité.php
+++ Public/Modele/classe_Activité.php
@@ -2,6 +2,7 @@
 class Activité { // activité d'un travail (TP/TD/contrôle...)
   private ID;
   private nom;
+  private description;
   
   public function __construct(){}
   public function __destruct(){}
