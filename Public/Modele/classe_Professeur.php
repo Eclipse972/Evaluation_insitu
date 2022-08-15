8fd4f9294f9565daf1b215dbb3b682500d69bebf
Christophe HERVI <christophe.hervi@free.fr>
2022-08-15 02:38:42 +0200

    indentation

--- Public/Modele/classe_Professeur.php
+++ Public/Modele/classe_Professeur.php
@@ -1,23 +1,22 @@
 <?php
-class Professeur
-{
-private ID;
-private pseudo;
-private hashPWD;
-private nom;
-private prenom;
-private email;
+class Professeur {
+	private ID;
+	private pseudo;
+	private hashPWD;
+	private nom;
+	private prenom;
+	private email;
 
-function __construct() {
-}
+	function __construct() {
+	}
 
-function __destruct() {
-	// la suppression d'un profeseur peut avoir des conséquences sur d'autres objets
-}
-	
-// setter
+	function __destruct() {
+		// la suppression d'un profeseur peut avoir des conséquences sur d'autres objets
+	}
 
-// getter
-	
-// 
+	// setter
+
+	// getter
+
+	// Autre
 }
