a40d68633d6d3f1b63d097dfe47daf6827bdcd57
Christophe HERVI <christophe.hervi@free.fr>
2022-08-15 02:57:58 +0200

    créaion de a classe activité

---
+++ Public/Modele/classe_Activité.php
@@ -0,0 +1,12 @@
+<?php
+class Activité { // activité d'un travail (TP/TD/contrôle...)
+  private ID;
+  private nom;
+  
+  public function __construct(){}
+  public function __destruct(){}
+  // setter
+  // getter
+  // autre
+}
+
