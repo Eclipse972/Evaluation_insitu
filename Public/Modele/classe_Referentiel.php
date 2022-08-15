1a0664c265aced52655ab34494f91035975fdab2
Christophe HERVI <christophe.hervi@free.fr>
2022-08-15 02:52:27 +0200

    création classe référentiel

---
+++ Public/Modele/classe_Referentiel.php
@@ -0,0 +1,12 @@
+<?php
+class Referentiel {
+  private ID;
+  private nom;
+  private dateMAJ;
+  
+  public function __construct(){}
+  public function __destruct(){}
+  // setter
+  // getter
+  // autre
+}
