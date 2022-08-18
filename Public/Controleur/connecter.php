a49787f67a52178a75a125fcbf2f969b0c82399c
Christophe HERVI <christophe.hervi@free.fr>
2022-08-18 20:49:26 +0200

    connexion est un nom problém&tique Pour deux raison .htacces interdit l'accès aux fichiers portant ce nom .gitignore ne suis pas les fichiers se nommant connexion  Modifications qui seront validées : 	nouveau fichier : CSS/connecter.css 	nouveau fichier : Controleur/connecter.php 	renommé :         Vue/connexion.html -> Vue/connecter.html

---
+++ Public/Controleur/connecter.php
@@ -0,0 +1,7 @@
+<?php	// page de connexion
+$this->setTitle("&Eacute;valuation in situ");
+$this->setHeaderText("<p class=\"font-effect-outline\">&Eacute;valuation en temps réel</p>");
+$this->setLogo("logo.png");
+$this->setFooter(" - <a href=/Contact>Me contacter</a>");
+$this->setView("connecter.html");
+$this->setCSS(array("connecter"));
