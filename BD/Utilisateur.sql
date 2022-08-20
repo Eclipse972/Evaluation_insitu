adc5caca9982c4b210b39b7da80f418e0a9144d1
Christophe HERVI <christophe.hervi@free.fr>
2022-08-21 01:11:01 +0200

    Création de la table utilisateur Pour le moment le MDP est stocké en clair. Il sera remplacer par son hash dans le futur. Ajout du premier utilisateur qui servira pour le mode démonstration  Modifications qui seront validées : 	nouveau fichier : Utilisateur.sql

---
+++ BD/Utilisateur.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.5
+-- http://www.phpmyadmin.net
+--
+-- Serveur: c.hervi.sql.free.fr
+-- Généré le : Dim 21 Août 2022 à 01:08
+-- Version du serveur: 5.0.83
+-- Version de PHP: 5.3.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `c_hervi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Utilisateur`
+--
+
+DROP TABLE IF EXISTS `Utilisateur`;
+CREATE TABLE IF NOT EXISTS `Utilisateur` (
+  `ID` int(11) NOT NULL auto_increment,
+  `pseudo` varchar(99) collate latin1_general_ci NOT NULL,
+  `nom` varchar(99) collate latin1_general_ci NOT NULL,
+  `prenom` varchar(99) collate latin1_general_ci NOT NULL,
+  `MDP` text collate latin1_general_ci NOT NULL COMMENT 'hash dans le futur',
+  `courriel` varchar(99) collate latin1_general_ci NOT NULL,
+  PRIMARY KEY  (`ID`),
+  UNIQUE KEY `pseudo` (`pseudo`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `Utilisateur`
+--
+
+INSERT INTO `Utilisateur` (`ID`, `pseudo`, `nom`, `prenom`, `MDP`, `courriel`) VALUES
+(1, 'Démo', 'Arnaud', 'Nyme', 'mot2passe', 'arnaud.nyme@free.fr');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
