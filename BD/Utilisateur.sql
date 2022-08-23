b62437cb90cf04145d93051a3c0af960b8814d69
Christophe HERVI <christophe.hervi@free.fr>
2022-08-23 15:11:18 +0200

    ajout de Mickael et moi comme utilisateurs

--- BD/Utilisateur.sql
+++ BD/Utilisateur.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: c.hervi.sql.free.fr
--- Généré le : Dim 21 Août 2022 à 03:51
+-- Généré le : Mar 23 Août 2022 à 15:09
 -- Version du serveur: 5.0.83
 -- Version de PHP: 5.3.9
 
@@ -35,14 +35,16 @@ CREATE TABLE IF NOT EXISTS `Utilisateur` (
   `courriel` varchar(99) collate latin1_general_ci NOT NULL,
   PRIMARY KEY  (`ID`),
   UNIQUE KEY `pseudo` (`pseudo`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=4 ;
 
 --
 -- Contenu de la table `Utilisateur`
 --
 
 INSERT INTO `Utilisateur` (`ID`, `pseudo`, `nom`, `prenom`, `MDP`, `courriel`) VALUES
-(1, 'Démo', 'Arnaud', 'Nyme', 'mot2passe', 'arnaud.nyme@free.fr');
+(1, 'Démo', 'Nyme', 'Arnaud', 'mo2passe', 'arnaud.nyme@ac-amiens.fr'),
+(2, 'Eclypse972', 'HERVI', 'Christophe', 'spirx', 'christophe.hervi@ac-amiens.fr'),
+(3, 'Micka', 'ROUSSEL', 'Mickael', 'Christine', 'mickael.roussel1@ac-amiens.fr');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
