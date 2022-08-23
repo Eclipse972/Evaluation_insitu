1ecd58f01514100ab125b2ecbdb93685a3c82eaa
Christophe HERVI <christophe.hervi@free.fr>
2022-08-23 15:14:40 +0200

    petites corrections de texte dans la page démo

--- Public/Controleur/demo.php
+++ Public/Controleur/demo.php
@@ -1,12 +1,12 @@
 <?php
 ob_start();	// début du code <section>
 ?>
-	<h1>Le mode d&eacute;monstration</h1>
+	<h1>K&eacute;sako?</h1>
 	<p>Ce mode permet d&apos;explorer toutes les possibilit&eacute;s de l&apos;application. La seule
 		différence avec un utilisateur enregistr&eacute; est que vous ne pourrez cr&eacute;er/modifier/effacer
 		aucune donn&eacute;e.
 	</p>
-	<p><a href=/professeur>Aller &agrave; la page Professeur</a></p>
+	<p><a href=/professeur>page Professeur en mode d&eacute;mo</a></p>
 	<p><a href=/connecter>Vous connecter</a></p>
 <?php
 $tampon = ob_get_contents();
