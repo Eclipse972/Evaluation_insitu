6df0c7ae5239353c88bfc32ffd9cbe38a9aa0f1f
Christophe HERVI <christophe.hervi@free.fr>
2022-08-23 11:21:36 +0200

    page d'aide prof

---
+++ Public/Controleur/aideProf.php
@@ -0,0 +1,24 @@
+<?php
+$this->setHeaderText("Aide");
+$this->setCSS([]);
+
+ob_start();	// début du code <section>
+?>
+	<h1>La page professeur</h1>
+	<p>Cette page est d&eacute;coup&eacute;e en plusieurs parties</p>
+	<ul>
+		<li>Profil: </li>
+		<li>Classes: </li>
+		<li>Devoirs: </li>
+		<li>Comp&eacute;tences: </li>
+		<li>&Eacute;valuations: </li>
+	</ul>
+	<p><a href=/professeur>Retour &agrave; la page Professeur</a></p>
+<?php
+$tampon = ob_get_contents();
+ob_end_clean();
+
+$this->setSection($tampon);
+
+$this->setFooter("");
+$this->setView("doctype.html");
