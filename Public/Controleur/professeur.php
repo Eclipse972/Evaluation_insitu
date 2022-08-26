4fb17ed7616c30a5779e57a56009d6016d6004fd
Christophe HERVI <christophe.hervi@free.fr>
2022-08-26 03:11:07 +0200

    profID -> IDprof pour ressembler au champ de BDD + modification de la liste des rubriques

--- Public/Controleur/professeur.php
+++ Public/Controleur/professeur.php
@@ -1,23 +1,23 @@
 <?php // controleur de la page professeur
-$IDprof = isset($_SESSION["IDprof"]) ? $_SESSION["IDprof"] : 1; // identifiant du prof actuel
+$profID = isset($_SESSION["profID"]) ? $_SESSION["profID"] : 1; // identifiant du prof actuel
 
 $this->setCSS(["professeur"]);
 
 ob_start();	// début du code <section>
 ?>
 <h1>Profil</h1>
-<p>Pseudo: <?=PEUNC\BDD::SELECT("pseudo FROM Utilisateur WHERE ID = ?", [$IDprof])?></p>
-<p>Nom: <?=PEUNC\BDD::SELECT("nom FROM Utilisateur WHERE ID = ?", [$IDprof])?></p>
-<p>Pr&eacute;nom: <?=PEUNC\BDD::SELECT("prenom FROM Utilisateur WHERE ID = ?", [$IDprof])?></p>
-<p>Courriel: <?=PEUNC\BDD::SELECT("courriel FROM Utilisateur WHERE ID = ?", [$IDprof])?></p>
+<p>Pseudo: <?=PEUNC\BDD::SELECT("pseudo FROM Utilisateur WHERE ID = ?", [$profID])?></p>
+<p>Nom: <?=PEUNC\BDD::SELECT("nom FROM Utilisateur WHERE ID = ?", [$profID])?></p>
+<p>Pr&eacute;nom: <?=PEUNC\BDD::SELECT("prenom FROM Utilisateur WHERE ID = ?", [$profID])?></p>
+<p>Courriel: <?=PEUNC\BDD::SELECT("courriel FROM Utilisateur WHERE ID = ?", [$profID])?></p>
+
+<h1>Vos classes</h1>
+<p>En construction ...</p>
 
 <h1>Vos devoirs</h1>
 <p>En construction ...</p>
 
 <h1>Vos comp&eacute;tences</h1>
-<p>En construction ...</p>
-
-<h1>Vos classes</h1>
 <p>En construction ...</p>
 
 <h1>Vos &eacute;valuations</h1>
@@ -33,6 +33,7 @@ ob_start();	// début du code <nav>
 <a href=#>Devoirs</a>
 <a href=#>Comp&eacute;tences</a>
 <a href=#>&Eacute;valuer</a>
+<a href=#>Synth&egrave;ses</a>
 <a href=/deconnexion>D&eacute;connexion</a>
 <?php
 $this->setNav(ob_get_contents());
