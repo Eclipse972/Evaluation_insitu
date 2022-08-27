8769a8ac0ffdbee52c36b71129b954fafee39f7e
Christophe HERVI <christophe.hervi@free.fr>
2022-08-27 04:50:23 +0200

    premère implémentation de la classe Professeur

--- Public/Controleur/professeur.php
+++ Public/Controleur/professeur.php
@@ -1,51 +1,17 @@
 <?php // controleur de la page professeur
-$profID = isset($_SESSION["profID"]) ? $_SESSION["profID"] : 1; // identifiant du prof actuel
+$Oprof = new Professeur(isset($_SESSION["profID"]) ? $_SESSION["profID"] : 1);
 
 $this->setCSS(["professeur"]);
 // $essai = PEUNC\BDD::SELECT("pseudo_ FROM Utilisateur WHERE ID = ?", [$profID]);
 
-// list($pseudo, $nom, $prenom, $courriel) = ... à partir de PHP7
-$Treponse	= PEUNC\BDD::SELECT("pseudo, nom, prenom, courriel FROM Utilisateur WHERE ID = ?", [$profID]);
-$pseudo		= $Treponse["pseudo"];
-$nom		= $Treponse["nom"];
-$prenom		= $Treponse["prenom"];
-$courriel	= $Treponse["courriel"];
+$this->setSection(	$Oprof->AfficherProfil()
+				.	$Oprof->AfficherClasses()
+				.	$Oprof->AfficherDevoirs()
+				.	$Oprof->AfficherCompetences()
+				.	$Oprof->AfficherEvaluations()
+			);
 
-ob_start();	// début du code <section>
-?>
-<h1>Profil</h1>
-<p>Pseudo: <?=$pseudo?></p>
-<p>Nom: <?=$nom?></p>
-<p>Pr&eacute;nom: <?=$prenom?></p>
-<p>Courriel: <?=$courriel?></p>
-
-<h1>Vos classes</h1>
-<p>En construction ...</p>
-
-<h1>Vos devoirs</h1>
-<p>En construction ...</p>
-
-<h1>Vos comp&eacute;tences</h1>
-<p>En construction ...</p>
-
-<h1>Vos &eacute;valuations</h1>
-<p>En construction ...</p>
-<?php
-$this->setSection(ob_get_contents());
-ob_end_clean();
-
-ob_start();	// début du code <nav>
-?>
-<a href=/professeur/modifier>Modifier profil</a>
-<a href=#>Classes</a>
-<a href=#>Devoirs</a>
-<a href=#>Comp&eacute;tences</a>
-<a href=#>&Eacute;valuer</a>
-<a href=#>Synth&egrave;ses</a>
-<a href=/deconnexion>D&eacute;connexion</a>
-<?php
-$this->setNav(ob_get_contents());
-ob_end_clean();
+$this->setNav(Professeur::AfficherMenu());
 
 $this->setFooter("professeur");
 $this->setView("doctype.html");
--- Public/Modele/classe_Professeur.php
+++ Public/Modele/classe_Professeur.php
@@ -1,16 +1,76 @@
 <?php
 class Professeur extends PEUNC\User
 {
-	function __construct() {
+	public function __construct($profID = 1)
+	{	// hydratation
+		$this->ID = $profID;
+		// list($pseudo, $nom, $prenom, $courriel) = ... à partir de PHP7
+		$Treponse = PEUNC\BDD::SELECT("pseudo, nom, prenom, courriel FROM Utilisateur WHERE ID = ?", [$profID]);
+		$this->pseudo	= $Treponse["pseudo"];
+		$this->nom		= $Treponse["nom"];
+		$this->prenom	= $Treponse["prenom"];
+		$this->courriel	= $Treponse["courriel"];
 	}
 
-	function __destruct() {
+	public function __destruct()
+	{
 		// la suppression d'un profeseur peut avoir des conséquences sur d'autres objets
 	}
 
 	// setter
 
-	// getter
+	// GETTERS ====================================================================================
 
-	// Autre
+	// Autre ======================================================================================
+	public function AfficherProfil()
+	{
+		return	"<h1>Profil</h1>\n"
+			.	"<p>Pseudo: "		. $this->pseudo		. "</p>\n"
+			.	"<p>Nom: " 			. $this->nom		. "</p>\n"
+			.	"<p>Pr&eacute;nom: ". $this->prenom		. "</p>\n"
+			.	"<p>Courriel: "		. $this->courriel	. "</p>\n";
+	}
+
+	public function AfficherClasses()
+	{
+		$code = "<h1>Vos classes</h1>\n";
+		// génération de la liste
+		$code .= "<p>En construction ...</p>\n";
+		return $code;
+	}
+
+	public function AfficherCompetences()
+	{
+		$code = "<h1>Vos comp&eacute;tences</h1>\n";
+		// génération de la liste
+		$code .= "<p>En construction ...</p>\n";
+		return $code;
+	}
+
+	public function AfficherEvaluations()
+	{
+		$code = "<h1>Vos &eacute;valuations</h1>\n";
+		// génération de la liste
+		$code .= "<p>En construction ...</p>\n";
+		return $code;
+	}
+
+	public function AfficherDevoirs()
+	{
+		$code = "<h1>Vos devoirs</h1>\n";
+		// génération de la liste
+		$code .= "<p>En construction ...</p>\n";
+		return $code;
+	}
+
+	public static function AfficherMenu()
+	{
+		return	"<a href=/professeur/modifier>Modifier profil</a>\n"
+			.	"<a href=#>Classes</a>\n"
+			.	"<a href=#>Devoirs</a>\n"
+			.	"<a href=#>Comp&eacute;tences</a>\n"
+			.	"<a href=#>&Eacute;valuer</a>\n"
+			.	"<a href=#>Synth&egrave;ses</a>\n"
+			.	"<a href=/deconnexion>D&eacute;connexion</a>\n";
+	}
 }
