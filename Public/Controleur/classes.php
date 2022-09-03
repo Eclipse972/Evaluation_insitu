0af132a5fc5d1095efd266a97b5101e5f4d7bcc6
Christophe HERVI <christophe.hervi@free.fr>
2022-09-03 20:34:15 +0200

    choisir une classe dans la liste

--- Public/Controleur/classes.php
+++ Public/Controleur/classes.php
@@ -1,10 +1,27 @@
 <?php
-$this->setSection(Professeur::AfficherRubrique("G&eacute;rer ce petit monde", "Vue_Prof_groupes",
-												isset($_SESSION["profID"]) ? $_SESSION["profID"] : 1));
-$this->setNav("<a href=#>Cr&eacute;er</a>\n"
-			. "<a href=#>Ajouter</a>\n"
-			. "<a href=#>Modifier</a>\n"
-			. "<a href=#>Supprimer</a>\n");
+
+$profID = isset($_SESSION["profID"]) ? $_SESSION["profID"] : 1;
+
+// création de la liste des liens vers les classes
+$ReponseSQL = PEUNC\BDD::SELECT("nom FROM Vue_Prof_groupes WHERE profID = ?", [$profID]);
+
+switch(PEUNC\BDD::SELECT("count(*) FROM Vue_Prof_groupes WHERE profID = ?", [$profID]))
+{	// cette partie est factorisable pour créer les listes pour les devoirs, les référentiels, ...
+	case 0:		// aucune réponse
+		$code = "";
+		break;
+	case 1:		// réponse unique
+		$code = "<a href=#>{$ReponseSQL}</a>\n";
+		break;
+	default:	// construction de la liste
+		$code = "";
+		foreach($ReponseSQL as $valeur)	$code .= "<a href=#>{$valeur["nom"]}</a>\n";
+}
+
+// construction de la page
+$this->setSection("<h1>Choisissez ...</h1>\n<p><a href=/professeur>ou revenir en arri&egrave;re</a></p>\n");
+
+$this->setNav($code . "<a href=#>Cr&eacute;er</a>\n" . "<a href=#>Ajouter</a>\n");
 
 $this->setFooter("");
 $this->setView("doctype.html");
