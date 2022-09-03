67ea6114c31564d6e8735ba15c16a60c7afd9787
Christophe HERVI <christophe.hervi@free.fr>
2022-09-03 20:49:13 +0200

    ajout de l'ID de l'objet en lien avec le professeur

--- BD/Vue_Prof_devoirs.sql
+++ BD/Vue_Prof_devoirs.sql
@@ -1,7 +1,7 @@
 -- Liste des devoirs par prof
 DROP VIEW IF EXISTS Vue_Prof_devoirs;
 CREATE VIEW Vue_Prof_devoirs AS
-SELECT profID, nom
+SELECT profID, nom, Devoir.ID
 FROM Prof_Devoir
 INNER JOIN Devoir ON Devoir.ID=Prof_Devoir.devoirID
 ORDER BY profID, nom;
--- BD/Vue_Prof_groupes.sql
+++ BD/Vue_Prof_groupes.sql
@@ -1,7 +1,7 @@
 -- Liste des groupes par prof
 DROP VIEW IF EXISTS Vue_Prof_groupes;
 CREATE VIEW Vue_Prof_groupes AS
-SELECT profID, nom
+SELECT profID, nom, Groupe.ID
 FROM Prof_Groupe
 INNER JOIN Groupe ON Groupe.ID=Prof_Groupe.groupeID
 ORDER BY profID, nom;
