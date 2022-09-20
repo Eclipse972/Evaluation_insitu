814de4f9d5ff5ae11fa79897f459e4d7ad6ab8ca
Christophe HERVI <christophe.hervi@free.fr>
2022-09-21 00:17:27 +0200

    suppression de l'item créer dans les menus

--- Public/Controleur/referentiels.php
+++ Public/Controleur/referentiels.php
@@ -5,7 +5,7 @@ $profID = isset($_SESSION["profID"]) ? $_SESSION["profID"] : 1;
 // construction de la page
 $this->setSection("<h1>Choisissez ...</h1>\n<p><a href=/professeur>ou revenir en arri&egrave;re</a></p>\n");
 
-$this->setNav(CollectionReferentiel::Liste($profID) . "<a href=#>Cr&eacute;er</a>\n<a href=#>Ajouter</a>\n");
+$this->setNav(CollectionReferentiel::Liste($profID) . "<a href=#>Ajouter</a>\n");
 
 $this->setFooter("");
 $this->setView("doctype.html");
