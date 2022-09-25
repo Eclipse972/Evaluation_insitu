3261cc2676cb60bb99fbeb34c24409664abb83a9
Christophe HERVI <christophe.hervi@free.fr>
2022-09-25 18:58:37 +0200

    sauvegarde de la table squelette au 25/9/2022

--- BD/Squelette.sql
+++ BD/Squelette.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: evaluation.insitu.sql.free.fr
--- Généré le : Mer 21 Septembre 2022 à 00:25
+-- Généré le : Dim 25 Septembre 2022 à 18:56
 -- Version du serveur: 5.0.83
 -- Version de PHP: 5.3.9
 
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `Squelette` (
   `paramAutorise` varchar(99) collate latin1_general_ci NOT NULL default '[]' COMMENT 'syntaxe JSON',
   PRIMARY KEY  (`ID`),
   UNIQUE KEY `navigation` (`alpha`,`beta`,`gamma`,`methode`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=16 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=17 ;
 
 --
 -- Contenu de la table `Squelette`
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `Squelette` (
 
 INSERT INTO `Squelette` (`ID`, `alpha`, `beta`, `gamma`, `texteMenu`, `imageMenu`, `ptiNom`, `classePage`, `controleur`, `methode`, `paramAutorise`) VALUES
 (5, 0, 0, 0, 'Accueil', '', 'home', 'Page', 'home.php', 'GET', '[]'),
-(6, 1, 0, 0, 'Connexion', '', 'connecter', 'Page', 'connecter.php', 'GET', '[]'),
+(6, 1, 0, 0, 'Connexion', '', 'connecter', 'PEUNC\\Formulaire', 'connecter.php', 'GET', '[]'),
 (7, 2, 0, 0, 'Professeur', '', 'professeur', 'Page', 'professeur.php', 'GET', '[]'),
 (8, 1, 1, 0, 'Pb de connexion?', '', 'aide', 'Page', 'aideConnexion.php', 'GET', '[]'),
 (9, 2, 1, 0, 'Aide professeur', '', 'aide', 'Page', 'aideProf.php', 'GET', '[]'),
@@ -56,7 +56,8 @@ INSERT INTO `Squelette` (`ID`, `alpha`, `beta`, `gamma`, `texteMenu`, `imageMenu
 (12, 4, 0, 0, 'D&eacute;connexion', '', 'deconnexion', 'Page', 'deconnecter.php', 'GET', '[]'),
 (13, 5, 0, 0, 'Vos groupes/classes', '', 'classes', 'Page', 'classes.php', 'GET', '[]'),
 (14, 6, 0, 0, 'Devoirs', '', 'devoirs', 'Page', 'devoirs.php', 'GET', '[]'),
-(15, 7, 0, 0, 'R&eacute;f&eacute;rentiels', '', 'referentiels', 'Page', 'referentiels.php', 'GET', '[]');
+(15, 7, 0, 0, 'R&eacute;f&eacute;rentiels', '', 'referentiels', 'Page', 'referentiels.php', 'GET', '[]'),
+(16, 1, 0, 0, 'Connexion', '', 'connecter', 'PEUNC\\Formulaire', 'connecter.php', 'POST', '["CSRF", "utilisateur", "MDP"]');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
