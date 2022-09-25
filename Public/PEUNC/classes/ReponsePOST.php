ec25cb08612e85d6b86bfc9e59314bcfafd064da
Christophe HERVI <christophe.hervi@free.fr>
2022-09-25 21:24:31 +0200

    réponse POST vide por le moment

--- Public/PEUNC/classes/ReponsePOST.php
+++ Public/PEUNC/classes/ReponsePOST.php
@@ -8,17 +8,18 @@ class ReponsePOST extends ReponseClient
 	public function __construct(HttpRouter $route)
 	{
 		parent::__construct($route);
-		$this->Pretraitement();
-
-		// traitement du formulaire
-		$formulaire = new $classePage(
+		$formulaire = new $this->classePage(
 								$route->getAlpha(),
 								$route->getBeta(),
 								$route->getGamma(),
 								"POST",
-								$this->TlisteReponses
+								$this->PrepareParametres($_POST)
 							);
-		if ($formulaire->FormulaireOK())
+
+		header("Location:/professeur"); // redirection vers la page prof sans traitement pour le moment
+
+		// traitement du formulaire		
+/*		if ($formulaire->FormulaireOK())
 		{
 			$formulaire->Traitement();
 			$URL = $formulaire->URLprecedente();
@@ -29,24 +30,6 @@ class ReponsePOST extends ReponseClient
 			$URL = BDD::SELECT("URL FROM Vue_Routes WHERE niveau1 = ? AND niveau2 = ? AND niveau3 = ? AND methodeHttp = ?", [$formulaire->getAlpha(), $formulaire->getBeta(), $formulaire->getGamma(), "GET"]);
 		}
 		header("Location:" . $URL); // redirection
-	}
-
-	public function Pretraitement()	// pré-traitement commun à tous les formulaires
-	{
-		$this->TlisteReponses = $this->PrepareParametres($_POST);
-
-		if (isset($this->TlisteReponses["CSRF"]))
-		{
-			$jetonChiffré = $this->TlisteReponses["CSRF"];
-			require"config_chiffrement.php";	// défini $cipher, $key et $iv
-			$jetonJSON = openssl_decrypt($jetonChiffré, $cipher, $key, $options=0, $iv);
-			
-			$O_jeton = json_decode($jetonJSON);
-			if (!isset($O_jeton))
-				throw new ApplicationException("Jeton CSRF corrompu");
-			elseif (time() - $O_jeton->depart < 5)
-				throw new ApplicationException("temps de r&eacute;ponse inhumain");
-		}
-		else throw new ApplicationException("Jeton CSRF absent");
+*/
 	}
 }
