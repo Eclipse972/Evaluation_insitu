4aa077279e72eecc4391f66b4564c81dcd8fbc7b
Christophe HERVI <christophe.hervi@free.fr>
2022-10-02 00:54:26 +0200

    création de compte

---
+++ Public/Controleur/creerCompte.php
@@ -0,0 +1,5 @@
+<?php // créer un compte
+$this->setSection("<h1>En construction</h1>\n<p>Envoyez moi un courriel pour cr&eacute;er votre compte en cliquant sur l&apos;ic&ocirc;ne de messagerie ci-dessous.</p>\n");
+$this->setNav("");
+$this->setFooter("");
+$this->setView("doctype.html");
