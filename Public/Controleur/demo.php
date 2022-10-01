c9506111f2dacc3a6ffc502eb2d3a7a5a6891d87
Christophe HERVI <christophe.hervi@free.fr>
2022-10-02 00:56:02 +0200

    ajout des choix dans le menu

--- Public/Controleur/demo.php
+++ Public/Controleur/demo.php
@@ -2,15 +2,11 @@
 ob_start();	// début du code <section>
 ?>
 	<h1>K&eacute;sako?</h1>
-	<p>Ce mode permet d&apos;explorer toutes les possibilit&eacute;s de l&apos;application. La seule
-		différence avec un utilisateur enregistr&eacute; est que vous ne pourrez cr&eacute;er/modifier/effacer
-		aucune donn&eacute;e.
-	</p>
-	<p><a href=/professeur>page Professeur en mode d&eacute;mo</a></p>
-	<p><a href=/connecter>Vous connecter</a></p>
+	<p>Ce mode permet d&apos;explorer toutes les possibilit&eacute;s de l&apos;application.</p>
+	<p>La seule diff&eacute;rence avec un utilisateur enregistr&eacute; est que vous ne pourrez cr&eacute;er/modifier/effacer aucune donn&eacute;e.</p>
 <?php
 $this->setSection(ob_get_contents());
 ob_end_clean();
-
+$this->setNav("<a href=/professeur>Rester en mode d&eacute;mo</a>\n<a href=/connecter>Vous identifier</a>\n");
 $this->setFooter("");
 $this->setView("doctype.html");
--- Public/Controleur/home.php
+++ Public/Controleur/home.php
@@ -3,12 +3,11 @@ ob_start();	// début du code <section>
 ?>
 <h1>Bienvenu.e</h1>
 <p>L&apos;objectif de site est de pouvoir &eacute;valuer en situation &agrave; l&apos;aide de son smartphone.</p>
-<p>Envoyez-moi un courriel pour <a href="mailto:christophe.hervi@ac-amiens.fr?subject=Créer un compte">cr&eacute;er un compte</a></p>
+<p>Le mode d&eacute;mo permet d'acc&eacute;der &agrave; toutes le fontionalit&eacute;s du site.</p>
 <?php
 $this->setSection(ob_get_contents());
 ob_end_clean();
 
-$this->setNav("<a href=/connecter>CONNEXION</a>\n<a href=/demonstration>D&Eacute;MO</a>\n");
-
+$this->setNav("<a href=/connecter>Se connecter</a>\n<a href=/creerCompte>Cr&eacute;er un compte</a>\n<a href=/demonstration>Mode d&eacute;monstration</a>\n");
 $this->setFooter("");
 $this->setView("doctype.html");
