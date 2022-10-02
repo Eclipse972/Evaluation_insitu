c3d2a8ddb371844422ab66ebc6a2d1e856f53901
Christophe HERVI <christophe.hervi@free.fr>
2022-10-02 14:00:06 +0200

    supression de professeur.css

--- Public/Controleur/professeur.php
+++ Public/Controleur/professeur.php
@@ -1,11 +1,7 @@
 <?php // controleur de la page professeur
 $Oprof = new Professeur(isset($_SESSION["profID"]) ? $_SESSION["profID"] : 1);
 
-$this->setCSS("professeur");
-
 $this->setSection($Oprof->AfficherProfil());
-
 $this->setNav(Professeur::AfficherMenu());
-
 $this->setFooter("professeur");
 $this->setView("doctype.html");
